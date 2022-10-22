dfd562d4b34912b1beabf519cc1d35bb540dbf53
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-10-22 11:18:34 +0900

    Add files via upload

--- Day02/MainActivity.kt
+++ Day02/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.hyunpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                // to do.. -클릭을하면 어떤 것을 하고 싶은지 적어줌.
+            }
+        })
+
+
+        /* - 버튼에 람다식을 사용 할 수 있는 경우
+         1. Kotlin interface가 아닌 자바 인터페이스여야 합니다.
+         2. 그 인터페이스는 딱 하나의 메소드만 가져야 합니다. */
+        button.setOnClickListener {
+            // to do.. -클릭을하면 어떤 것을 하고 싶은지 적어줌.
+        }
+
     }
 }
