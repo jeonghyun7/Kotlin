092c366eb8bb5bb3d5d2a3a213fd5f2fde8f9e98
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-10-27 12:25:09 +0900

    Add files via upload

---
+++ ACADEMY/Day02/Day02_exam05.kt
@@ -0,0 +1,42 @@
+
+
+fun main() {
+
+/*산술 연산자
+  var n = 10
+
+    if ((n % 2) == 1) { // 홀수
+        println("n is an Odd number")
+    }
+    if ((n % 2) == 0) { // 짝수
+        println("n is an Even number")
+    }
+*/
+
+
+/*대입 연산자
+    num = num +2 // 산술 연산자와 대입 연산자를 함께 사용하는 경우
+    num += 2 // 이렇게 간략하게 표현
+*/
+
+
+/* 증가 연산자와 감소 연산자
+    var num1 = 10
+    var num2 = 10
+    val result1 = ++num1 // num 값 증가 후 대입
+    val result2 = num2++ // 먼저 num 값 대입 후 증가
+
+    println("result1: $result1")
+    println("result2: $result2")
+    println("num1: $num1")
+    println("num2: $num2")
+*/
+
+
+/* 논리 연산자
+    var check = (5>3) && (5>2) // 2개의 항((5>3), (5>2))이 모두 참이면 true
+    check = (5>3) || (2>5)  // 2개 중 1개의 항이 참이면 true
+    check = !(5>3)  // true는 false로, false는 true로 변경
+*/
+
+}
---
+++ ACADEMY/Day02/Day02_exam06.kt
@@ -0,0 +1,15 @@
+/* 변수와 함수
+    - 함수 선언하기
+
+// 일반적인 함수의 모습
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+// 간략화된 함수
+fun sum(a: Int, b: Int): Int = a + b
+
+// 반환 자료형 생략
+fun sum(a: Int, b: Int) = a + b
+
+*/
