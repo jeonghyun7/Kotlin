83c89db74c739e79d224e29acefeaa4195423dc4
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-10-28 12:46:51 +0900

    Add files via upload

---
+++ ACADEMY/Day03/Day03_WhileFactorial.kt
@@ -0,0 +1,13 @@
+/* while문으로 활용한 팩토리얼 계산하기 */
+
+fun main() {
+    print("Enter the number: ")
+    var number = readLine()!!.toInt()
+    var factorial: Long = 1
+
+    while (number > 0) {
+        factorial *= number
+        --number
+    }
+    println("Factorial: $factorial")
+}
